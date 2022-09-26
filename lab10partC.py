084d743d4819cf876171edae1f637c867a0aa1f8
rhoouma <71633763+rhoumaesen@users.noreply.github.com>
2022-09-26 15:40:01 +0000

    lab13 stuff

--- lab10partC.py
+++ lab10partC.py
@@ -1,11 +1,15 @@
 from cryptosteganography import CryptoSteganography
+
+
 key = "1111222233334444!"
 crypto_steganography = CryptoSteganography(key)
 print()
+
+################# transmitter 
 print('The program is looking for an image named ch6_secret_image.png\n')
-origfile = "chapter6\steg\ch6_secret_image.png"
+origfile = "nebula.png"
 print('The image with the hidden message will be called steg_ch6_secret_image.png\n')
-modfile = "chapter6\steg\steg_ch6_secret_image.png"
+modfile = "mnebula_m.png"
 secretMsg = ""
 message1 = "Sympathy for the favorite nation, facilitating the illusion of an imaginary common "
 message2 = "interest in cases where no real common interest exists, and infusing into one the "
@@ -13,11 +17,15 @@ message3 = "enmities of the other, betrays the former into a participation in th
 message4 = "wars of the latter without adequate inducement or justification."
 secretMsg = secretMsg.join([message1, message2, message3, message4])
 crypto_steganography.hide(origfile, modfile, secretMsg)
+
+
+#### receiver ###############333
 secret = crypto_steganography.retrieve(modfile)
 print("The secret that is hidden in the file is:\n")
 print(secret)
 print()
 
+################### wrong receiver #########################
 print('Now we will try the wronge secret.\n')
 key = "AnotherKey"
 crypto_steganography = CryptoSteganography(key)
