6fb422b3c33358544dfe2108e10ef25c1ed35964
rhoouma <71633763+rhoumaesen@users.noreply.github.com>
2023-02-15 09:22:21 +0000

    lab1 remove bcrypt

--- lab1.py
+++ lab1.py
@@ -1,4 +1,3 @@
-
 ################ Question 1 ##################################
 def reverseCipher(plaintext):
   ciphertext = ''
@@ -19,48 +18,46 @@ print(recovered)
 
 ############################################################
 
+
 ################ Question 2 ##################################
+
+
 import getpass
-import bcrypt
-
-
-
  
 def ask_for_username():
       print("enter the user name would you like to use...")
       username = input()
       return username
  
- 
 
 def ask_for_password():
      while True:
       print("What password would you like to create?")
-      salt=bcrypt.gensalt()
+     # salt=bcrypt.gensalt()
       p=getpass.getpass()
       inputstr=str.encode(p)      
-      hashed_password1 = bcrypt.hashpw(inputstr,salt)
-      print(hashed_password1)
+     # hashed_password1 = bcrypt.hashpw(inputstr,salt)
+      print(p)
       
       print("Please enter the password again...")
       q=getpass.getpass()
       inputstr2=str.encode(q)
-      hashed_password2 = bcrypt.hashpw(inputstr,salt)
-      print(hashed_password2)
+     # hashed_password2 = bcrypt.hashpw(inputstr,salt)
+      print(q)
        
-      if hashed_password1 == hashed_password2:
+      if p == q:
         print("Your password is matching...")
-        print("Your username, hashed password and salt is stored in testhash.txt file")        
-        return hashed_password2, salt
+        print("Your username and password are stored in testhash.txt file")        
+        return q
       else:
         print("Your password do not match. Please retry...")
 
-def store_info(username, hashed_pass, salt):
+def store_info(username, password):
 #password_file =open_pass_file()
   with open("testhash.txt", "a" ) as password_file:
-    password_file.write(username  + " | " + "".join(map(chr,hashed_pass ))   + " | " + "".join(map(chr, salt )) + "\n" + "\n")
+    password_file.write(username  + " | " + password + "\n" + "\n")
    
     
 username = ask_for_username()
-hashedPass, salt = ask_for_password()
-store_info(username, hashedPass, salt)
+password = ask_for_password()
+store_info(username, password )
